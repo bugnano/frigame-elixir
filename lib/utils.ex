93e49bc43ca6b088722f9644577a9ae83731489b
fri <fri>
2025-12-29 09:50:57 +0100

    More idiomatic clamp function

--- lib/utils.ex
+++ lib/utils.ex
@@ -1,6 +1,5 @@
 defmodule FriGame.Utils do
   def clamp(n, min_val, max_val)
-      when is_number(n) and is_number(min_val) and is_number(max_val) do
-    min(max(n, min_val), max_val)
-  end
+      when is_number(n) and is_number(min_val) and is_number(max_val),
+      do: n |> max(min_val) |> min(max_val)
 end
