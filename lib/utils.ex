7795d5f40d7b3227fd66a9492095cb228ae935cb
fri <fri>
2026-07-14 07:25:56 +0200

    Added lerp function

--- lib/utils.ex
+++ lib/utils.ex
@@ -2,4 +2,8 @@ defmodule FriGame.Utils do
   def clamp(n, min_val, max_val)
       when is_number(n) and is_number(min_val) and is_number(max_val),
       do: n |> max(min_val) |> min(max_val)
+
+  # t = (x - x0) / (x1 - x0)
+  def lerp(y0, y1, t) when is_number(y0) and is_number(y1) and is_number(t),
+    do: y0 + t * (y1 - y0)
 end
